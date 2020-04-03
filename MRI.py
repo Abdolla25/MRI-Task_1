6fa58184048320172e05edd91dcd93f0d11d4e7b
Shaimaa Gamal <shymaagamal666@gmail.com>
2020-04-03 23:38:31 +0200

    done Fourier Transform and plot random wave in z direction

---
+++ MRI.py
@@ -0,0 +1,43 @@
+###########################################################################################################################################
+# Project : Task 1
+# File Name: MRI..py
+# Description: Bloch Equation Simulation
+# Author: Team ---
+########################################################################################################################################
+
+
+
+
+
+import numpy as np
+import cv2 as cv
+import matplotlib.pyplot as p
+from numpy import random
+def main ():
+    img=cv.imread('1.jpg',0)
+    spectrum =np.fft.fftshift(np.fft.fft2(img))
+    p.subplot(321)
+    p.imshow(img, cmap='gray')
+    p.title('input image') 
+    p.subplot(322)
+    p.imshow( np.log(np.abs(spectrum)) , cmap='gray')
+    p.title('Magnitude Spectrum') 
+    p.subplot(323)
+    p.imshow(np.angle(spectrum),cmap='gray')
+    p.title('Phase Spectrum') 
+    p.subplot(324)
+    p.imshow(np.real(spectrum),cmap='gray')
+    p.title('real Spectrum') 
+    p.subplot(325)
+    p.imshow(  np.imag(spectrum),cmap='gray')
+    p.title('imaginary Spectrum') 
+    p.show()
+############################################   Plot Random Variable     ################################################################
+    z = random.random(200)
+    p.ylabel("Z axis")
+    p.xlabel("Time")
+    p.plot(z)
+    p.show()
+    
+if __name__=="__main__":
+    main()
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# MRI-Task_1
+### Bloch Equation Simulation
+
+# Result
+>### **the Fourier transform ofthe image and its four component**
+* ![](comp.jpeg)
+
+>### **This Function that rotates around Z axis only**
+* ![](surface.png)
+
